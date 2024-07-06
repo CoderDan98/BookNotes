68ff615f72e45c404c022a3bc36369f9d4e51334
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-07-06 10:04:52 +0100

    Loading State

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import BookCard from "./components/BookCard/BookCard";
 
 const App = () => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,6 +16,8 @@ const App = () => {
         setBooks(response.data);
       } catch (error) {
         console.error("Error fetching the book data", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -26,17 +29,21 @@ const App = () => {
       <Header />
       <div className="content">
         <h1>Book List</h1>
-        <div className="book-list">
-          {books.map((book, index) => (
-            <BookCard
-              key={index}
-              title={book.title}
-              description={book.description}
-              releaseDate={book.releaseDate}
-              pageCount={book.pageCount}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="book-list">
+            {books.map((book, index) => (
+              <BookCard
+                key={index}
+                title={book.title}
+                description={book.description}
+                releaseDate={book.release_date}
+                pageCount={book.page_count}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </div>
