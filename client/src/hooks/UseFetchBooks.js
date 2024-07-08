7bb736ba88d4425c93dffa121aba74f66f7c5e44
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-07-08 16:01:13 +0100

    refetchBooks method

--- client/src/hooks/UseFetchBooks.js
+++ client/src/hooks/UseFetchBooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { FetchBooks } from "../services/Books";
 
 const UseFetchBooks = () => {
@@ -6,24 +6,25 @@ const UseFetchBooks = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const GetBooks = async () => {
-      try {
-        const data = await FetchBooks();
-        setBooks(data);
-        setError(null);
-      } catch (error) {
-        console.error("Error fetching the book data", error);
-        setError("Failed to fetch book data. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    GetBooks();
+  const GetBooks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const data = await FetchBooks();
+      setBooks(data);
+      setError(null);
+    } catch (error) {
+      console.error("Error fetching the book data", error);
+      setError("Failed to fetch book data. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
-  return { books, loading, error };
+  useEffect(() => {
+    GetBooks();
+  }, [GetBooks]);
+
+  return { books, loading, error, refetch: GetBooks };
 };
 
 export default UseFetchBooks;
