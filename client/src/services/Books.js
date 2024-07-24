14b4ef9f25d31bc0441cc1a2236463a3cda4cbbe
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-07-24 09:07:23 +0100

    URL Update

--- client/src/services/Books.js
+++ client/src/services/Books.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance with a base URL
 const api = axios.create({
-  baseURL: "https://book-notes-application-server.onrender.com/books",
+  baseURL: "https://booknotesappserver.danielsarney.com//books",
 });
 
 // Function to fetch all books
